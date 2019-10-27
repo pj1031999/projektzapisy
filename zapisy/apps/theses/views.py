659893c89ec554ca3d31b930ad4699b5d04abf52
Jasiek Busz <jasiekbusz@gmail.com>
2019-10-27 12:56:39 +0100

    Displaying all theses in list_all.html

--- zapisy/apps/theses/views.py
+++ zapisy/apps/theses/views.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.urls import reverse
 from django.db.models import Q
 from django.shortcuts import render
+from apps.theses.models import Thesis
 
 
 def list_all(request):
-    """
-        Temporarily list all theses
-    """
+    theses = Thesis.objects.all()
 
-    return render(request, 'theses/list_all.html')
+    return render(request, 'theses/list_all.html', {
+        'theses': theses,
+    })
