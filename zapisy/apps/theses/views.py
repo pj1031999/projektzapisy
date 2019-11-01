017925aadfe1ad35b8e177ebbb964ca1b0e2d52a
barnij <barnij@windowslive.com>
2019-11-01 13:27:32 +0100

    Możliwość przeglądania prac tylko gdy zaloowany

--- zapisy/apps/theses/views.py
+++ zapisy/apps/theses/views.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.urls import reverse
 from django.db.models import Q
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from apps.theses.models import Thesis
+from apps.theses.enums import ThesisKind
 
-
+@login_required
 def list_all(request):
     theses = Thesis.objects.all()
 
@@ -13,12 +15,14 @@ def list_all(request):
     })
 
 
+@login_required
 def view_thesis(request, id):
     """
         Show subpage for one thesis
     """
 
     query = Thesis.objects.filter(id=id)
+    thesiskind = {int(i):i.display for i in ThesisKind}
     thesis = None if len(query)==0 else query[0]
 
-    return render(request, 'theses/thesis.html', {'thesis': thesis})
+    return render(request, 'theses/thesis.html', {'thesis': thesis, 'thesiskind': thesiskind})
