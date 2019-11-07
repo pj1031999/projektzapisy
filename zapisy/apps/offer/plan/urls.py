f1e0c3db63127a5f81f89d0f1d63bba61081d06d
Karol Guzikowski <karolguzikowski@gmail.com>
2019-11-07 17:42:06 +0100

    plan routes, views and templates full setup

--- zapisy/apps/offer/plan/urls.py
+++ zapisy/apps/offer/plan/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.plan_main, name='plan-main'),
+    path('', views.plan_view, name='plan-view'),
+    path('create/', views.plan_create, name='plan-create'),
 ]
--- zapisy/apps/offer/plan/views.py
+++ zapisy/apps/offer/plan/views.py
@@ -1,10 +1,16 @@
-from django.contrib import messages
-from django.db import models
 from django.shortcuts import render, HttpResponse
+from apps.users.models import BaseUser
 
 
-def plan_main(request):
+def plan_view(request):
+    if request.user.is_superuser or BaseUser.is_employee(request.user):
+        return render(request, 'plan/view-plan.html')
+    else:
+        return HttpResponse(status=403)
+
+
+def plan_create(request):
     if request.user.is_superuser:
-        return render(request, 'plan/plan-main.html')
+        return render(request, 'plan/create-plan.html')
     else:
         return HttpResponse(status=403)
