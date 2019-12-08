96e66035f28044e795f4b75a69b2dbbc45bfd19b
pala19 <39733330+pala19@users.noreply.github.com>
2019-12-08 17:46:34 +0100

    poprawa błędów(flake8)

--- zapisy/apps/grade/tickets/models/private_key.py
+++ zapisy/apps/grade/tickets/models/private_key.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-
 class PrivateKey(models.Model):
-    poll = models.ForeignKey('poll.Poll', verbose_name='ankieta', on_delete=models.CASCADE)
+    poll = models.ForeignKey('poll.Poll',
+                             verbose_name='ankieta', on_delete=models.CASCADE)
     private_key = models.TextField(verbose_name='klucz prywatny')
 
-     class Meta:
+    class Meta:
         verbose_name = 'klucz prywatny'
         verbose_name_plural = 'klucze prywatne'
         app_label = 'tickets'
--- zapisy/apps/grade/tickets/utils.py
+++ zapisy/apps/grade/tickets/utils.py
@@ -26,7 +26,7 @@ def generate_keys(poll_list):
 
 def save_keys(polls_with_keys):
     # ewentualnie rozdzielić na dwie osobne funkcje dla private and public
-    for poll, keys in polls_with_keys: 
+    for poll, keys in polls_with_keys:
         private_key = PrivateKey(poll=poll, private_key=keys[0])
         public_key = PublicKey(poll=poll, public_key=keys[1])
         private_key.save()
@@ -36,9 +36,9 @@ def save_keys(polls_with_keys):
 def generate_key():
     # jakaś zmiana, by nie podawać tego tak jawnie
     length = 1024
-    key = RSA.generate(length) 
-    private_key = key.exportKey('PEM').decode(encoding='ascii', errors='strict')
-    public_key = key.publickey().exportKey('PEM').decode(encoding='ascii', errors='strict')
+    key = RSA.generate(length)
+    private_key = key.exportKey('PEM').\
+        decode(encoding='ascii', errors='strict')
+    public_key = key.publickey().exportKey('PEM').\
+        decode(encoding='ascii', errors='strict')
     return (private_key, public_key)
-
-
