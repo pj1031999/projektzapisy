b0f2e060067d8de37f76fd36314a0f7ba96bfeb1
pala19 <39733330+pala19@users.noreply.github.com>
2019-12-08 17:26:40 +0100

    stworzenie modeli private_key oraz public_key

---
+++ zapisy/apps/grade/tickets/models/private_key.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+
+class PrivateKey(models.Model):
+    poll = models.ForeignKey('poll.Poll', verbose_name='ankieta', on_delete=models.CASCADE)
+    private_key = models.TextField(verbose_name='klucz prywatny')
+
+     class Meta:
+        verbose_name = 'klucz prywatny'
+        verbose_name_plural = 'klucze prywatne'
+        app_label = 'tickets'
+
+    def __str__(self):
+        return f'Klucz prywatny: {self.poll}'
+
+    @staticmethod
+    def _int_from_bytes(xbytes: bytes) -> int:
+        return int.from_bytes(xbytes, 'big')
