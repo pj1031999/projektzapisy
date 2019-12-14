6f94debd5508d960697540a7e97995afb7acc628
pala19 <39733330+pala19@users.noreply.github.com>
2019-12-14 14:35:06 +0100

    zmiana modeli private_key i public_key na model rsa_keys

---
+++ zapisy/apps/grade/tickets/models/rsa_keys.py
@@ -0,0 +1,26 @@
+from django.db import models
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+
+
+class RSAkeys(models.Model):
+    poll = models.ForeignKey('poll.Poll',
+                             verbose_name='ankieta', on_delete=models.CASCADE)
+    private_key = models.TextField(verbose_name='klucz prywatny')
+    public_key = models.TextField(verbose_name='klucz publiczny')
+
+    class Meta:
+        verbose_name = 'klucze RSA'
+        verbose_name_plural = 'klucze RSA'
+        app_label = 'tickets'
+
+    def __str__(self):
+        return f'Klucze RSA: {self.poll}'
+
+    def sign_ticket(self, ticket):
+        key = RSA.importKey(self.private_key)
+        ticket_hash = SHA256.new(ticket)
+        signature = PKCS1_v1_5.new(key).sign(ticket_hash)
+        sign_as_int = int.from_bytes(signature, 'big')
+        return sign_as_int
