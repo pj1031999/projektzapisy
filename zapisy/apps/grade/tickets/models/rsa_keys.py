d37649981ee1ff86c863480bcfcfe839b063c609
pala19 <39733330+pala19@users.noreply.github.com>
2020-01-31 11:16:10 +0100

    dodanie metody statycznej parse_raw_tickets

--- zapisy/apps/grade/tickets/models/rsa_keys.py
+++ zapisy/apps/grade/tickets/models/rsa_keys.py
@@ -1,7 +1,10 @@
+import json
 from django.db import models
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 from Crypto.Signature import PKCS1_v1_5
+from apps.grade.poll.models import Poll
+from apps.grade.tickets.models.used_ticket import UsedTicket
 
 
 class RSAKeys(models.Model):
@@ -27,6 +30,54 @@ class RSAKeys(models.Model):
             'n': str(key.n),
             'e': str(key.e),
         }
+
+    @staticmethod
+    def parse_raw_tickets(raw_tickets):
+        """Parses raw json containing tickets.
+        Raises:
+            JSONDecodeError: when provided string is not in json format.
+            ValueError: when there is something wrong with internal ticket format,
+                for example, some of the requred fields are not provided, type
+                of field is incorrect, there are duplicate ids, or id does not
+                exist in database.
+        Returns:
+            Tuple of (valid_polls, error_polls), where error_polls are list of those
+            polls, for which signature was incorrect, and valid_polls is list of named tuples,
+            containing two fields, ticket_id and poll, where ticket_id can be used to
+            track which ticket has already been used to vote.
+        """
+        tickets = json.loads(raw_tickets)
+        tickets_list = tickets['tickets']
+
+        tickets_ids = [ticket['id'] for ticket in tickets_list]
+
+        # Make sure there are no duplicate ids
+        if len(tickets_ids) != len(set(tickets_ids)):
+            raise ValueError("Duplicate ids detected")
+
+        polls = Poll.objects.filter(pk__in=tickets_ids).select_related('rsakeys').order_by('pk')
+        # Make sure all provided ids exist in database
+        if len(polls) != len(tickets_list):
+            raise ValueError("Provided id doesn't exist in database")
+        tickets_list.sort(key=lambda ticket: ticket['id'])
+
+        valid_polls = []
+        error_polls = []
+        used_polls = []
+
+        for poll, tickets in zip(polls, tickets_list):
+            keys = RSAKeys.objects.get(poll=poll)
+            ticket = int(tickets['ticket'].encode())
+            signed_ticket = int(tickets['signature'].encode())
+            if UsedTicket.was_ticket_used(poll, ticket, signed_ticket):
+                used_polls.append(poll)
+            elif keys.verify_ticket(signed_ticket, ticket):
+                valid_polls.append((tickets['ticket'], poll))
+                used_ticket = UsedTicket(poll=poll, ticket=ticket, signed_ticket=signed_ticket)
+                used_ticket.save()
+            else:
+                error_polls.append(poll)
+        return valid_polls, error_polls, used_polls
 
     def sign_ticket(self, ticket):
         key = RSA.importKey(self.private_key)
