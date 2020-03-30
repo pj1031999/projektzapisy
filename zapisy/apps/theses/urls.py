c7f8cced03fa1cb4e4d99cc0d8d415ae4a1e3923
Jasiek Busz <jasiekbusz@gmail.com>
2020-03-30 16:39:53 +0200

    Update to Django 3.0.4

--- zapisy/apps/theses/urls.py
+++ zapisy/apps/theses/urls.py
@@ -4,11 +4,11 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register("theses", views.ThesesViewSet, base_name="theses")
-router.register("theses_board", views.ThesesBoardViewSet, base_name="theses_board")
-router.register("theses_employees", views.EmployeesViewSet, base_name="theses_employees")
-router.register("theses_ac_students", views.StudentAutocomplete, base_name="theses_ac_students")
-router.register("theses_ac_employees", views.EmployeeAutocomplete, base_name="theses_ac_employees")
+router.register("theses", views.ThesesViewSet, basename="theses")
+router.register("theses_board", views.ThesesBoardViewSet, basename="theses_board")
+router.register("theses_employees", views.EmployeesViewSet, basename="theses_employees")
+router.register("theses_ac_students", views.StudentAutocomplete, basename="theses_ac_students")
+router.register("theses_ac_employees", views.EmployeeAutocomplete, basename="theses_ac_employees")
 
 urlpatterns = [
     path("", views.theses_main, name="main"),
