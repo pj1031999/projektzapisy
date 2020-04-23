ac48abb5484cd7926a073b1102b9b9f5b8d1962f
Paweł Jasiak <pawel@jasiak.xyz>
2020-04-23 19:56:57 +0200

    dos2unix env/env3_setup.sh

--- env/env3_setup.sh
+++ env/env3_setup.sh
@@ -1,7 +1,6 @@
 # set up virtualenv
-cd $HOME
-python3.6 -m venv env3
-source env3/bin/activate
+python3.6 -m venv $HOME/env3
+source $HOME/env3/bin/activate
 
 # The version of python we've built won't necessarily
 # have the latest version of pip bundled with it
