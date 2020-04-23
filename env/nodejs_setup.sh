4e482f98229ef90d93dbb39550bb4fffffc91ce7
Paweł Jasiak <pawel@jasiak.xyz>
2020-04-23 19:48:01 +0200

    Remove sudo from env/nodejs_setup.sh
    In Vagrantfile we don't manipulate _privileged_ flag for env/nodejs_setup.sh so
    we don't need to use sudo command here.
    
    According to the documentation:
    > privileged (boolean) - Specifies whether to execute the shell script as a
    > privileged user or not (sudo). By default this is "true". Windows guests use
    > a scheduled task to run as a true administrator without the WinRM limitations.

--- env/nodejs_setup.sh
+++ env/nodejs_setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Install nodejs
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get install -y nodejs
 
 # Install yarn, needed globally
-sudo npm i -g yarn
+npm i -g yarn
